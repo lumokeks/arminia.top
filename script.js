8f9b03a3aada070bbdd6694203352f58a760c6ff
lumokeks <lumokeks@gmail.com>
2025-01-24 17:05:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ function CreatePopup() {
     __P.innerHTML = `<b>STOP!</b><br>
                     Wenn sie auf OK! drücken, gelangen sie zum Prototyp meines Stundenplans.
                     Dieser ist nicht funktionstüchtig und soll lediglich das Design veranschaulichen.<br>
-                    (Das Design ist noch an meine Bildschirmeinstellungen angepasst und deshalb könnten manche Farben zu Dunkel angezeigt werden.)<br><br>
+                    (Das Design ist noch an meine Bildschirmeinstellungen angepasst und deshalb könnten manche Farben zu Dunkel angezeigt werden.)<br>
                     Sorry!`;
     const __ActionNavigator = CreateElement(__ContentContainer, "div", ".action-navigator");
     const __Item = CreateElement(__ActionNavigator, "a", ".item");
