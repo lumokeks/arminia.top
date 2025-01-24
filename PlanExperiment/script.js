880260bb67f6839478662f52b0e0314a98923978
lumokeks <lumokeks@gmail.com>
2025-01-24 17:13:14 +0100

    Update script.js

--- PlanExperiment/script.js
+++ PlanExperiment/script.js
@@ -120,10 +120,9 @@ class UIElement {
                 function __blur() {Animate(__Close, {}, {background: "none"}, 40);};
                 __Close.addEventListener("mouseenter", () => __focus());
                 __Close.addEventListener("mouseleave", () => __blur());
-                new UIElement(true, "button-click", __Close, __focus(), ...args);
+                new UIElement(true, "button-click", __Close, ...args);
             },
             "button-click": (...args) => {
-                let focused = false;
                 function __focus () {focused = true; Animate(args[0], {}, {transform: "translateY(2px)"}, 40);};
                 function __blur() {focused = false; Animate(args[0], {}, {transform: "translateY(0px)"}, 40);};
                 args[0].addEventListener("mouseleave", () => __blur());
