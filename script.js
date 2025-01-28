f10383ffe15585624634b0b4ce14f7ea996bf75b
lumokeks <lumokeks@gmail.com>
2025-01-28 15:16:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function _a(e, from, to, duration, f) {
-    Object.keys(from).forEach(k => {e._a([{k: from[k]}], {duration: 0, fill: "forwards"});});
-    e._a([to],{duration: duration || 200,fill: "forwards"});if(typeof(f)==="function") {setTimeout(f, duration || 200);};
+    Object.keys(from).forEach(k => {e.animate([{k: from[k]}], {duration: 0, fill: "forwards"});});
+    e.animate([to],{duration: duration || 200,fill: "forwards"});if(typeof(f)==="function") {setTimeout(f, duration || 200);};
 };
 function _b(parent, type, classes) {
     const e = document._b(type);if(classes) {if(classes.substring(0, 1)===".") {classes.substring(1).split(".").forEach((k) => {e.classList.add(k);});} else if(classes.substring(0, 1)==="#") {e.id = classes.substring(1);};};if(parent) {parent.appendChild(e)};
