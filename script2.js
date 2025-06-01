359a62f9a26ceb139ba08c257fb5ac50099a3bbd
lumokeks <lumokeks@gmail.com>
2025-06-01 02:03:50 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -6,36 +6,6 @@ function Animate(element, from, to, duration, f) {  Object.keys(from).forEach(k 
         element.animate([{k: from[k]}], {duration: 0, fill: "forwards"});
     });element.animate([to],{duration: duration || 200,fill: "forwards"});if(typeof(f)==="function") {setTimeout(f, duration || 200);};   };
 function CSSVar(data) {    const style = getComputedStyle(document.body);return style.getPropertyValue(data);   }; // Gets CSS Variables
-
-// // self-made sudoku solver using random number generation (working on a solver using the backtracking algorithm!!)
-// class Sudoku {
-//     constructor(grid) {
-//         grid = (grid||[]);for(let i = grid.length; i < 9; i++) grid.push([]);this.preset = [];
-//         for(let i = 0; i < grid.length; i++) for(let i2 = 0; i2 < grid.length; i2++) grid[i][i2]&&this.preset.push(`${i},${i2}`);
-//         this.safe_copy = (a) => {return JSON.parse(JSON.stringify(a));};this.grid = grid;
-//     };
-//     is_valid(grid, _nv, _mv, _b) {
-//         function __return(a, cr, cc, t) {return {a: a, b: Number(cr), _mv: Number(cc), _b: _b};};
-//         for(let i = 0; i < grid.length; i++) {let c1 = grid[i][_mv]===_b, c2 = grid[_nv][i]===_b;
-//             if(c1||c2) return __return(false, c1&&String(i)||String(_nv), c2&&String(i)||String(_mv));};
-//         let rS = Math.floor(_nv / 3) * 3, cS = Math.floor(_mv / 3) * 3;
-//         for(let r2 = rS; r2 < rS + 3; r2++) for(let c2 = cS; c2 < cS + 3; c2++) if(grid[r2][c2]===_b) return __return(false, r2, c2);
-//         return __return(true, _nv, _mv);
-//     };
-//     solve(f2) {
-//         function __s(self, _a) {let __f = [1, 2, 3, 4, 5, 6, 7, 8, 9];function solve_row(self, _nv) {
-//                     if(_nv>=_a.length) return;for(let _mv = 0; _mv < _a.length; _mv++) {if(!_a[_nv][_mv]) {let __a = [];
-//                             __f.forEach(e => self.is_valid(_a, _nv, _mv, e).a&&__a.push(self.is_valid(_a, _nv, _mv, e)));
-//                             if(__a.length) {_a[_nv][_mv] = __a[Math.floor(Math.random() * __a.length)]._b;} else return;};};
-//                 };
-//                 for(let i2 = 0; i2 < 9; i2++) solve_row(self, i2);
-//                 let f = true;
-//                 for(let i2 = 0; i2 < _a.length; i2++) for(let i3 = 0; i3 < _a.length; i3++) {if(!_a[i2][i3]) {f = false;};};
-//                 if(f) {return _a;} else {return;};};
-//         for(let i = 0; i < 10000; i++) { // recalling function everytime grid isn't complete (max. 10000 times)
-//                 let g = __s(this, this.safe_copy(this.grid));if(g) {return g};};
-//     };
-// };
 // sudoku solver using backtracking algorithm
 class _c14fake {
     constructor() {
@@ -88,7 +58,22 @@ function _c13(config) {
     __f3.innerHTML = config.data;
     return a;
 };
-setTimeout(() => _c13({title: "Test", data: "Test"}).show(), 2000);
+class _c12 {
+    constructor(config) {
+        let _a = Animate, _b = CreateElement;
+        let sf = document.querySelector("#system-feedback.content.container");
+        if(!sf) {sf = _b(document.body, "div", "#system-feedback--item-container");};
+        var __d11 = _b(sf, "div", ".s-feedback--container")
+        ,__d12 = _b(__d11, "div", ".type")
+        ,__d13 = _b(__d11, "div", ".content.container")
+        ,__e1 = _b(__d13, "p", ".title.font-bold.c-white")
+        ,__e2 = _b(__d13, "p", ".data.font-normal.c-shade");__d11.classList.add("popup-bg");__d11.classList.add("border");
+        __d11.setAttribute("type", config.type);__e1.textContent = config.title;__e2.textContent = config.data;__d11.style.height = `${(__d13.clientHeight - 36) + 50}px`
+        _a(__d11, {}, {transform: "translateX(-5%)", opacity: 1}, 100, () => {_a(__d11, {}, {transform: "translateX(0%)"}, 60);});
+        setTimeout(() => {_a(__d11, {}, {opacity: 0}, 100);_a(__d11, {}, {transform: "translateX(100%)"}, 100, () => {_a(__d11, {}, {height: "0px"}, 100, () => __d11.remove());});}, config.duration||6000);
+        if(sf.clientHeight>window.innerHeight) {Object.values(sf.children)[0].remove();};}
+};
+setTimeout(() => _c13({title: "Instructions", data: "Click on any cell to select it, next cell gets selected automatically.<br>0 to ignore selected cell and move on to the next cell (essentially creating an empty cell).<br>When last column in last row is filled, it will try to solve the grid (just FYI, using an illegal grid (e.g. two 1s in a single row) will result in this page getting frozen :D)"}).show(), 2000);
 class Sudoku {
     constructor(board) {
         this.board = board||[];
@@ -115,21 +100,6 @@ const col = (a) => {return CreateElement(CreateElement(a, "div", ".col.flex.x-ce
 function update_grid() {let a = window.innerWidth, b = window.innerHeight;document.querySelector(":root").style.setProperty("--grid-size", `${(a>b&&b||a) - 100}px`);};
 window.onresize = update_grid;
 update_grid();
-// function vis_grid(t, hl) {
-//     hl = hl||[];
-//     let _a = Object.values(document.querySelectorAll(".col > span")), __a = -1;
-//     if(!_a.length) {
-//         _a = [];
-//         for(let i = 0; i < 9; i++) {
-//             if(i%3===0) {if(__a>=0) __a++;__a++;};
-//             let a = grid(), b = undefined, _r = Math.floor(i/3)*9*3/9;
-//             for(let i2 = 0; i2 < 9; i2++) {if(i2%3===0) {b = row(a);};let c = col(b);c.parentElement.setAttribute("i", (__a * 9) + (i * 3) + i2 + Math.floor(i2 / 3) * 6);_a.push(c);};
-//         };
-//     };
-//     _a.sort((a, b) => Number(a.parentElement.getAttribute("i")) - Number(b.parentElement.getAttribute("i")));
-//     const classes = ["marked"]; function __g(e) {classes.forEach(e2 => e.classList.remove(e2));};
-//     for(let r = 0; r < 9; r++) for(let c = 0; c < 9; c++) {let h = _a[r*9+c].parentElement;h.setAttribute("r", r);h.setAttribute("c", c);_a[r*9+c].textContent = t[r][c]||"";__g(h);if(hl.find(e => e===`${r},${c}`)) h.classList.add("marked");};
-// };
 function vis_grid(t, m, init) {m = m||[]; init = init||[];
     const __f = (a, b) => {if(a===true) return {r: Math.floor(b / 9), c: b % 9}; else return a*9+b};let _a = document.querySelectorAll(".col > span");
     if(!_a.length) {_a = [];for(let i = 0; i < 9; i++) {let a = grid();for(let i2 = 0; i2 < 3; i2++) {let b = row(a);for(let i3 = 0; i3 < 3; i3++) {
