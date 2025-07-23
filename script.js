cb9d55da7588a9e65bd27ff8627f1a7c60686cd6
lumokeks <lumokeks@gmail.com>
2025-07-23 23:26:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ class _c14fake {
         };
     }
     _f1(parent, id) {
-        const __a = _b(undefined, "a");__a.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="${self.svgs[id]}"/></svg>`;
+        const __a = _b(undefined, "a");__a.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="${this.svgs[id]}"/></svg>`;
         return __a.querySelector("svg");
     }
 }
